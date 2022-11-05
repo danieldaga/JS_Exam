ac32b97ab8f8522336b6e27bfd46faab5036e30a
danieldaga <daniel@arhrestauracion.com>
2022-11-05 10:46:46 +0100

    exercise 5

---
+++ main.js
@@ -0,0 +1,54 @@
+//Exercise 1
+
+const array = [1, 2, 3, 4, 5, 6]
+
+function sumEvens(arr) {
+    let sumResult = []
+    arr.forEach(e => {
+
+    });
+}
+sumEvens(array)
+
+//Exercise 2
+
+function listNum(num) {
+    for (let i = 0; i < num + 1; i++) {
+        console.log(i);
+    }
+}
+listNum(12)
+
+//exercise 3
+
+function listNumdec(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+listNumdec(12)
+
+//exercise 4
+
+function listEvens(num) {
+    for (let i = 1; i < num + 1; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+listEvens(8)
+
+//exercise 5
+
+function multTable(num) {
+    let result 
+    for (let i = 0; i < 11; i++) {
+        result = i * num
+        console.log(`${i} x ${num} = ${result}`);
+    }
+}
+multTable(7)
+
+//exercise 6 
